a0f3313cce89f8e5abaa7c5a48a066a2070ebc58
kanglg <kanglgi@163.com>
2017-03-19 19:13:32 +0800

    init commit

---
+++ springboot/src/main/java/com/shanxinj/Application.java
@@ -0,0 +1,15 @@
+package com.shanxinj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * application
+ * Created by kanglg on 2017/3/19.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
